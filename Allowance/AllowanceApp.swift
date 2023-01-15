3da40bb2ad175f434df8d0c7f2053bb09174466a
Seth Wright <seth.wright@willowtreeapps.com>
2023-01-15 08:02:39 -0500

    Initial Commit

---
+++ Allowance/AllowanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllowanceApp.swift
+//  Allowance
+//
+//  Created by Seth Wright on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct AllowanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
